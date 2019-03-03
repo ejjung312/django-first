ed199966e2bd38b32d446f9734b44e5d0234b4df
ejjung312 <ejjung312@gmail.com>
2019-03-03 19:09:26 +0900

    allowed hosts

--- test1/settings.py
+++ test1/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'test1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'ejjung312',
+        'PASSWORD': 'qwer1234',
+        'HOST': 'ejjung312.mysql.pythonanywhere-services.com',
     }
 }
 

dd1161f37e4ce9734f3f105ed986cbcbb0943658
ejjung312 <ejjung312@gmail.com>
2019-03-03 19:15:46 +0900

    change database

--- test1/settings.py
+++ test1/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'ejjung312$test1',
         'USER': 'ejjung312',
         'PASSWORD': 'qwer1234',
         'HOST': 'ejjung312.mysql.pythonanywhere-services.com',
